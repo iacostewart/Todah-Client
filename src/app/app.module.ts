88fef525862ac3153f793da6f4c578e9f23e5c7d
The Sharps <Sharp@Thes-MacBook-Air.local>
2018-03-13 16:20:03 -0400

    push for Ian, reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,13 @@ import { FaqComponent } from './components/faq/faq.component';
 import { AboutComponent } from './components/about/about.component';
 import { PricingComponent } from './components/pricing/pricing.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+
+
+// import { StartCampaignForm1Component } from './campaign/startCampaign/shared/start-campaign-form1/start-campaign-form1.component'
+
 
 
 @NgModule({
@@ -36,11 +43,15 @@ import { ContactComponent } from './components/contact/contact.component';
     AppRoutingModule,
     CommonModule,
     CampaignModule,
-    AdminModule
+    AdminModule,
+    HttpModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    
   ],
 
   providers: [],
 
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent ]
 })
 export class AppModule { }
