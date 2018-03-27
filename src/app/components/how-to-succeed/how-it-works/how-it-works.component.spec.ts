721ad1ca6d6fe436e51e2f03aef9339ac5887ab7
Adam Smith <adamws33@gmail.com>
2018-03-27 19:17:06 -0400

    initial commit

--- src/app/components/how-to-succeed/how-it-works/how-it-works.component.spec.ts
+++ src/app/components/how-to-succeed/how-it-works/how-it-works.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HowItWorksComponent } from './how-it-works.component';
+// import { HowItWorksComponent } from './how-it-works.component';
 
-describe('HowItWorksComponent', () => {
-  let component: HowItWorksComponent;
-  let fixture: ComponentFixture<HowItWorksComponent>;
+// describe('HowItWorksComponent', () => {
+//   let component: HowItWorksComponent;
+//   let fixture: ComponentFixture<HowItWorksComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HowItWorksComponent ]
-    })
-    .compileComponents();
-  }));
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ HowItWorksComponent ]
+//     })
+//     .compileComponents();
+//   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HowItWorksComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(HowItWorksComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
--- src/app/components/how-to-succeed/how-it-works/how-it-works.component.ts
+++ src/app/components/how-to-succeed/how-it-works/how-it-works.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-how-it-works',
-  templateUrl: './how-it-works.component.html',
-  styleUrls: ['./how-it-works.component.css']
-})
-export class HowItWorksComponent implements OnInit {
+// @Component({
+//   selector: 'app-how-it-works',
+//   templateUrl: './how-it-works.component.html',
+//   styleUrls: ['./how-it-works.component.css']
+// })
+// export class HowItWorksComponent implements OnInit {
 
-  constructor() { }
+//   constructor() { }
 
-  ngOnInit() {
-  }
+//   ngOnInit() {
+//   }
 
-}
+// }
