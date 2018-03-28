acd8a3a27dcc0f1093bb16e24df3bea509bc2a56
Adam Smith <adamws33@gmail.com>
2018-03-28 08:04:15 -0400

    styling and html updates

--- src/app/campaign/delete-campaign/delete-campaign.component.ts
+++ src/app/campaign/delete-campaign/delete-campaign.component.ts
@@ -7,13 +7,10 @@ import { Router } from '@angular/router';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { campaingID } from '../globals';
 // import { campaignId } from "../startCampaign/shared/start-campaign-form1/start-campaign-form1.component"
-
 let campIds = Number(window.localStorage.campId)+1
-
 interface CampId{
   campId: number;
 }
-
 @Component({
   selector: 'app-delete-campaign',
   templateUrl: './delete-campaign.component.html',
@@ -21,28 +18,23 @@ interface CampId{
   providers: [campaingID]
 })
 export class DeleteCampaignComponent implements OnInit {
-
   deleteForm: FormGroup;
-
   // tslint:disable-next-line:max-line-length
   constructor(private http: HttpClient, private ar: ActivatedRoute, private campaignService: CampaignService, private router: Router, private form: FormBuilder, public campaignID: campaingID) {
   this.deleteCamp();
         }
-
-
   ngOnInit() {
+    window.scrollTo(0,0);
   }
-
   deleteCamp() {
     this.deleteForm = this.form.group({
       'id': new FormControl
     });
   }
-
   
   onSubmit(){
-    console.log("delete?", campIds);
-    console.log("CAMPAIGN ID DELETE",this.campaignID.ID)
+    // console.log("delete?", campIds);
+    // console.log("CAMPAIGN ID DELETE",this.campaignID.ID)
     this.campaignService.deleteCampaign(campIds)
     .subscribe(() => {
       this.router.navigate(['/home'])
@@ -60,8 +52,6 @@ export class DeleteCampaignComponent implements OnInit {
   }
   //   .subscribe(
   //   () =>{console.log("LOCAL STORAGE", window.localStorage.campId)},
-
   //     () => {this.router.navigate(['/'])}
   //   )}
   // }
-  
