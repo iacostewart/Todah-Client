1b104a087fe6960dc30bac72ba0102597ba1a310
Adam Smith <adamws33@gmail.com>
2018-03-28 10:25:51 -0400

    styling and html updates

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
-app.get("*", function(req, res){
-  res.sendfile("./src/app/components/home/home.component.html"  )
-});
+// app.get("*", function(req, res){
+//   res.sendfile("./src/index.html"  )
+// })
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
